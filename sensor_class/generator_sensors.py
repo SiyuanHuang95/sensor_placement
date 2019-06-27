87f5d94521ca7508663222d2dd03af33467c7f96
AnfangRobkit <siyuan.huang@student.kit.edu>
2019-06-27 13:26:37 +0200

    change the sensor init position

--- sensor_class/generator_sensors.py
+++ sensor_class/generator_sensors.py
@@ -10,6 +10,7 @@ from mat import CMate
 
 class CSensorFactory:
     factories = {}
+    area_length = 10
 
     @staticmethod
     def create_sensor(name, parameters):
@@ -24,14 +25,21 @@ class CSensorFactory:
             yield random.choice(types).__name__
 
     @staticmethod
+    def get_position():
+        position = random.randint(-CSensorFactory.area_length, CSensorFactory.area_length)
+        while abs(position) < 1:
+            position = random.randint(-CSensorFactory.area_length, CSensorFactory.area_length)
+        return position
+
+    @staticmethod
     def sensor_dict(sensor_name):
         if sensor_name == 'CMate':
-            parameters = {'x': random.randint(-10, 10), 'y': random.randint(-10, 10), 'width': random.randint(3, 5),
+            parameters = {'x': CSensorFactory.get_position(), 'y': CSensorFactory.get_position(), 'width': random.randint(3, 5),
                           'length': random.randint(4, 6)}
         elif sensor_name == 'CLidar':
-            parameters = {'x': random.randint(-10, 10), 'y': random.randint(-10, 10), 'lidar_range': random.randint(3, 10),
+            parameters = {'x': CSensorFactory.get_position(), 'y': CSensorFactory.get_position(), 'lidar_range': random.randint(3, 10),
                           'angle_res': random.uniform(0.01, 0.1), 'max_angle': 3.14 * random.random(),
                           'rate': random.randint(10, 100)}
         elif sensor_name == 'CFence':
-            parameters = {'x': random.randint(-10, 10), 'y': random.randint(-10, 10), 'length': random.randint(3, 5)}
+            parameters = {'x': CSensorFactory.get_position(), 'y': CSensorFactory.get_position(), 'length': random.randint(3, 5)}
         return parameters
